ab5b759e5f78dfd67774ef8cadae51df215b6cf0
wuwentao <wuwentao04@meituan.com>
2020-11-30 21:40:39 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 set -ex
-rsync --exclude venv -r ../e-insight root@ty-1:/root/
-ssh ty-1 "cd e-insight && docker-compose -f insight.yml build "
+rsync --exclude venv --exclude grafana-storage  --exclude prometheus-data -r ../e-insight root@ty-1:/root/
+ssh ty-1 "cd e-insight && docker-compose -f insight.yml up -d  "
 #ssh ty-1 "cd e-insight && docker-compose -f insight.yml up -d "
 
